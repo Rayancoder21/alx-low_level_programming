8ccddd50ece18845ec731f3c24de4ba74dea6192
Rayancoder21 <rayan.h.khogali@gmail.com>
2023-10-09 12:40:45 -0700

    ..

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  main - enrty point
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
