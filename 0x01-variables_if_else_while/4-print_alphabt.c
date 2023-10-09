c94fce0180c22412440796c529ab54107753d21d
Rayancoder21 <rayan.h.khogali@gmail.com>
2023-10-09 13:00:14 -0700

    ..

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - enrty point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+		char i;
+
+		for (i = 'a' ; i <= 'z' ; i++)
+			if (i != 'q' && i != 'e')
+				putchar(i);
+		putchar('\n');
+		return (0);
+}
