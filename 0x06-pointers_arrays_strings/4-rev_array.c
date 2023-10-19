bea69f0d8438d8499d6462eacc95723f17aa82a1
Rayancoder21 <rayan.h.khogali@gmail.com>
2023-10-18 23:29:55 -0700

    l

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,7 +8,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int j;
 
-	for (i = 0 ; i <= n ; i++)
-		a[n - i] = a[i];
+	for (i = 0 ; i <= n / 2 ; i++)
+	{
+		if (n)
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
 }
