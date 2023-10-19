f558dfc9240cf3743170ba4e5833a67a73e0a08c
Rayancoder21 <rayan.h.khogali@gmail.com>
2023-10-19 06:04:33 -0700

    ;

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - function that convert lowercase with uppercase
+ * @s: parameter
+ * Return: the result
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
+	}
+	return (s);
+}
